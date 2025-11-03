99cc468b540f8fc9838b643bcd2ffea9fe28e5e1
NantenainaRochel2 <soniarimamys@gmail.com>
2025-11-03 05:04:13 +0100

    feat: ajout des fonctions soustraction et division

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ let shouldResetScreen = false;
 function updateDisplay() {
     document.getElementById('display').textContent = currentInput;
 }
+function soustraction(a, b) {  return a - b;}
+function division(a, b) {
+    if (b === 0) { return 'Erreur';  }
+    return a / b;
+}
+// Fonction pour gérer les opérations
+function setOperation(op) {
+    if (currentInput === '0') return;
+    if (operation !== null) calculate();
+    previousInput = currentInput;
+    operation = op;
+    shouldResetScreen = true;
+}
